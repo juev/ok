ac4aa7acec71f84ff5ac3909ed5468b919e896a8
Denis Evsyukov <denis@evsyukov.org>
2020-02-29 23:17:20 +0300

    [~] help

--- src/main.rs
+++ src/main.rs
@@ -2,47 +2,8 @@ extern crate getopts;
 
 use getopts::*;
 use std::env;
+use std::path::Path;
 use std::process::Command;
-
-fn print_usage() {
-    let brief = format!(
-"Usage: ok [options] <number> [script-arguments..]
-       ok command [options]
-       
-command (use one):
-  <number>            Run the <number>th command from the '.ok' file.
-  l, list             Show the list from the '.ok' file. Default command.
-  L, list-once        Same as list, but only show when pwd is different from when the list was last shown.
-  p, list-prompt      Show the list and wait for input at the ok-prompt (like --list and <number> in one command).
-  h, help             Show this usage page.
-
-options:
-  -c, --comment_align N  Level of comment alignment. See $_OK_COMMENT_ALIGN
-  -v, --verbose       Show more output, mostly errors. Also it shows environment-variables in this screen.
-  -q, --quiet         Only show really necessary output, so surpress echoing the command.
-  -f, --file <file>   Use a custom file instead of '.ok'; use '-' for stdin
-  -a, --alias <name>  When using 'ok' in an alias, <name> is used to keep the history correct when used with 'list-prompt'.
-  -V, --version       Show version number and exit
-  -h, --help          Show this help screen
-script-arguments:
-  ...                 These are passed through, when a line is executed (you can enter these too at the ok-prompt)
-
-  environment variables (used for colored output; current colors are shown):
-  _OK_C_HEADING      Color-code for lines starting with a comment (heading). Defaults to red.
-  _OK_C_NUMBER       Color-code for numbering. Defaults to cyan.
-  _OK_C_COMMENT      Color-code for comments after commands. Defaults to blue.
-  _OK_C_COMMAND      Color-code for commands. Defaults to color-reset.
-  _OK_C_PROMPT       Color-code for prompt (both input as command confirmation). Defaults to color for numbering.
-environment variables (other configuration):
-  _OK_COMMENT_ALIGN  Level (unset) of comment alignment. 0=no alignment, 1=align consecutive lines (Default), 2=including whitespace, 3 align all.
-  _OK_PROMPT         String (unset) used as prompt (both input as command confirmation). Defaults to '$ '.
-  _OK_PROMPT_DEFAULT Setting (unset) if the prompt is default shown. 1=use command list-prompt when issuing no command, otherwise use list.
-  _OK_VERBOSE        Level (unset) of feedback ok provides. 0=quiet, 1=normal, 2=verbose. Defaults to 1. Can be overriden with --verbose or --quiet.
-environment variables (for internal use):
-  _OK__LAST_PWD      Remember the path (/path/to/some/place/with/an/.ok/file) that was last listed, for use with the list-once command.
-  _OK__PATH_TO_ME    The path (/path/to/ok-bash) to the location of this script.");
-    print!("{}", &brief);
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -76,16 +37,24 @@ fn main() {
         Ok(m) => m,
         Err(f) => panic!(f.to_string()),
     };
+    let mut verbose = false;
+    if matches.opt_present("v") {
+        verbose = true;
+    }
+
     if matches.opt_present("h") {
-        print_usage();
+        print_usage(&verbose);
         return;
     }
 
-    let mut path = env::current_dir().unwrap();
+    let mut dir = env::current_dir().unwrap();
     let file = matches.opt_str("f").unwrap_or(".ok".to_string());
 
-    path.push(file);
-    // print!("{}", path.display());
+    dir.push(file);
+    let path = Path::new(dir.to_str().unwrap()).canonicalize().unwrap();
+
+    print!("{}", path.display());
+
     let output = if cfg!(target_os = "windows") {
         Command::new("cmd")
             .args(&["/C", "echo hello"])
@@ -104,8 +73,49 @@ fn main() {
         println!("Command executed with failing error code");
     }
 
+    println!();
     String::from_utf8(output.stdout)
         .unwrap()
         .lines()
         .for_each(|x| println!("{}", x));
+}
+
+fn print_usage(verbose: &bool) {
+    println!("Usage: ok [options] <number> [script-arguments..]
+       ok command [options]
+       
+command (use one):
+  <number>            Run the <number>th command from the '.ok' file.
+  l, list             Show the list from the '.ok' file. Default command.
+  L, list-once        Same as list, but only show when pwd is different from when the list was last shown.
+  p, list-prompt      Show the list and wait for input at the ok-prompt (like --list and <number> in one command).
+  h, help             Show this usage page.
+
+options:
+  -c, --comment_align N  Level of comment alignment. See $_OK_COMMENT_ALIGN
+  -v, --verbose       Show more output, mostly errors. Also it shows environment-variables in this screen.
+  -q, --quiet         Only show really necessary output, so surpress echoing the command.
+  -f, --file <file>   Use a custom file instead of '.ok'; use '-' for stdin
+  -a, --alias <name>  When using 'ok' in an alias, <name> is used to keep the history correct when used with 'list-prompt'.
+  -V, --version       Show version number and exit
+  -h, --help          Show this help screen
+script-arguments:
+  ...                 These are passed through, when a line is executed (you can enter these too at the ok-prompt)");
+
+    if *verbose {
+        println!("environment variables (used for colored output; current colors are shown):
+  _OK_C_HEADING      Color-code for lines starting with a comment (heading). Defaults to red.
+  _OK_C_NUMBER       Color-code for numbering. Defaults to cyan.
+  _OK_C_COMMENT      Color-code for comments after commands. Defaults to blue.
+  _OK_C_COMMAND      Color-code for commands. Defaults to color-reset.
+  _OK_C_PROMPT       Color-code for prompt (both input as command confirmation). Defaults to color for numbering.
+environment variables (other configuration):
+  _OK_COMMENT_ALIGN  Level (unset) of comment alignment. 0=no alignment, 1=align consecutive lines (Default), 2=including whitespace, 3 align all.
+  _OK_PROMPT         String (unset) used as prompt (both input as command confirmation). Defaults to '$ '.
+  _OK_PROMPT_DEFAULT Setting (unset) if the prompt is default shown. 1=use command list-prompt when issuing no command, otherwise use list.
+  _OK_VERBOSE        Level (unset) of feedback ok provides. 0=quiet, 1=normal, 2=verbose. Defaults to 1. Can be overriden with --verbose or --quiet.
+environment variables (for internal use):
+  _OK__LAST_PWD      Remember the path (/path/to/some/place/with/an/.ok/file) that was last listed, for use with the list-once command.
+  _OK__PATH_TO_ME    The path (/path/to/ok-bash) to the location of this script.");
+    }
 }
